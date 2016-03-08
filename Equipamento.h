62e039c093b2453c53eb1a54a3ffbcb173dd144b
Claudomiro Sales <claudomiro.sales@gmail.com>
2016-03-08 10:30:29 -0300

    Assinatura da sobrecarga dos operadores << e =
     friend ostream &operator<<(ostream &, const Equipamento &);
     
     
     const Equipamento &operator=(const Equipamento &);

--- Equipamento.h
+++ Equipamento.h
@@ -1,11 +1,22 @@
+#include <ostream>
+using std::ostream;
+
 #ifndef EQUIPAMENTO_H
 #define EQUIPAMENTO_H
 
 class Equipamento
 {
+    friend ostream &operator<<(ostream &, const Equipamento &);
+    
 public:
     Equipamento();
     ~Equipamento();
+    
+    const Equipamento &operator=(const Equipamento &);
+    
+private:
+
+    
 
 };
 
