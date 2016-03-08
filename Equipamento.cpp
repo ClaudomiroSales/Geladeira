e2e5d96c42a95f2dbc48a4546f0ea52f7f31a61f
Claudomiro Sales <claudomiro.sales@gmail.com>
2016-03-08 10:38:04 -0300

    Movendo de Geladeira para Equipamento
    Métodos que são de qualquer equipamento, como ligar, registrar
    manutenção

--- Equipamento.cpp
+++ Equipamento.cpp
@@ -1,10 +1,35 @@
 #include "Equipamento.h"
 
 Equipamento::Equipamento()
+:ultimaManutencao() //Chamando o construtor de cópia de Data, tem de colocar o parêntese mesmo que não tenha parâmetros
 {
+    onoff = false;
 }
 
 Equipamento::~Equipamento()
 {
+}
+
+void Geladeira::ligar( )
+{
+    if ( !onoff )
+    {
+        onoff = true;
+        cout << "O aparelho foi ligado\n";
+    }
+    else
+        cout << "Aparelho jah estava ligado" << '\n';    
+}
+
+
+void Geladeira::registarManutencao(int dia, int mes, int ano)
+{
+    ultimaManutencao.setDia(dia);
+    ultimaManutencao.setMes(mes);
+    ultimaManutencao.setAno(ano);
+    
+    cout << "A manutencao atual eh ";
+    ultimaManutencao.print();
+    cout << '\n';
 }
 
--- Geladeira.cpp
+++ Geladeira.cpp
@@ -4,9 +4,7 @@
 using std::cout;
 
 Geladeira::Geladeira()
-:ultimaManutencao() //Chamando o construtor de cópia de Data, tem de colocar o parêntese mesmo que não tenha parâmetros
 {
-    onoff = false;
     temperatura = 0;
     
     numResidentes = 0;
@@ -15,18 +13,6 @@ Geladeira::Geladeira()
 
 Geladeira::~Geladeira()
 {
-}
-
-
-void Geladeira::ligar( )
-{
-    if ( !onoff )
-    {
-        onoff = true;
-        cout << "O aparelho foi ligado\n";
-    }
-    else
-        cout << "Aparelho jah estava ligado" << '\n';    
 }
 
 
@@ -69,16 +55,6 @@ void Geladeira::mostrarStatus( ) const
         cout << "Geladeira desligada.\n";
 }
 
-void Geladeira::registarManutencao(int dia, int mes, int ano)
-{
-    ultimaManutencao.setDia(dia);
-    ultimaManutencao.setMes(mes);
-    ultimaManutencao.setAno(ano);
-    
-    cout << "A manutencao atual eh ";
-    ultimaManutencao.print();
-    cout << '\n';
-}
 
 void Geladeira::adicionarResidentes(const string &novoResidente)
 {
